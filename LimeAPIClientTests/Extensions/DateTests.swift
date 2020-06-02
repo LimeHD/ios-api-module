58d0a3275deedc390f4321ecc249a6199cfa8876
Alexander Algashev <algashev@mail.ru>
2020-06-02 15:19:37 +0300

    Добавить тесты для Date [#172813191]

---
+++ LimeAPIClientTests/Extensions/DateTests.swift
@@ -0,0 +1,24 @@
+//
+//  DateTests.swift
+//  LimeAPIClientTests
+//
+//  Created by Лайм HD on 02.06.2020.
+//  Copyright © 2020 Лайм HD. All rights reserved.
+//
+
+import XCTest
+@testable import LimeAPIClient
+
+class DateTests: XCTestCase {
+    var sut: Date!
+
+    func test_rfc3339String_createsCorrectString() throws {
+        self.sut = Date()
+        let timeZone = try XCTUnwrap(TimeZone(secondsFromGMT: 3.hours))
+        let formatter = RFC3339DateFormatter()
+        formatter.timeZone = timeZone
+        let expectedString = formatter.string(from: self.sut)
+        
+        XCTAssertEqual(self.sut.rfc3339String(for: timeZone), expectedString)
+    }
+}
