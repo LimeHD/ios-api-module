04ac9356007d0cf953e419c7a292a2bb8669db0a
Alexander Algashev <algashev@mail.ru>
2020-06-04 14:38:46 +0300

    Добавить описание LACDateInterval [#172813191]

--- LimeAPIClient/LACDateInterval.swift
+++ LimeAPIClient/LACDateInterval.swift
@@ -8,18 +8,25 @@
 
 import Foundation
 
+/// Временной интервал между начальной и конечной датой с заданным часовым поясом
 public struct LACDateInterval {
     var start: Date
     var end: Date
     var duration: TimeInterval = 0
     var timeZone = TimeZone.current
     
+    /// Инициализирует интервал с  начальной и конечной датой равной текущей дате, временным интервалом равным 0 и часовым поясом используемым системой (TimeZone.current)
     public init() {
         let date = Date()
         self.start = date
         self.end = date
     }
     
+    /// Инициализирует интервал с задаными начальной, конечной датой и часовым поясом
+    /// - Parameters:
+    ///   - start: начальная дата
+    ///   - end: конечная дата
+    ///   - timeZone: часовой пояс
     public init(start: Date, end: Date, timeZone: TimeZone) {
         self.start = start
         self.end = end
@@ -27,6 +34,11 @@ public struct LACDateInterval {
         self.timeZone = timeZone
     }
     
+    /// Инициализирует интервал с задаными начальной датой, продолжительностью и часовым поясом
+    /// - Parameters:
+    ///   - start: начальная дата
+    ///   - duration: продолжительность
+    ///   - timeZone: часовой пояс
     public init(start: Date, duration: TimeInterval, timeZone: TimeZone) {
         self.start = start
         self.end = start.addingTimeInterval(duration)
