891339d592fda27ff224d8389915fd4671f41c32
Alexander Algashev <algashev@mail.ru>
2020-06-05 13:30:52 +0300

    Добавить документацию на LACApp и LACDateInterval [#172813191]

--- LimeAPIClient/LACDateInterval.swift
+++ LimeAPIClient/LACDateInterval.swift
@@ -9,10 +9,24 @@
 import Foundation
 
 /// Временной интервал между начальной и конечной датой с заданным часовым поясом
+///
+/// Пример инициализации:
+/// ```
+/// // Начальная дата ранее текущей даты на 8 дней
+/// let startDate = Date().addingTimeInterval(-8.days)
+/// // Часовой пояс +3 часа
+/// let timeZone = TimeZone(secondsFromGMT: 3.hours) ?? TimeZone.current
+/// // Временной интервал продолжительностью 15 дней
+/// let dateInterval = LACDateInterval(start: startDate, duration: 15.days, timeZone: timeZone)
+/// ```
 public struct LACDateInterval {
+    /// Начальная дата
     var start: Date
+    /// Конечная дата
     var end: Date
+    /// Продолжительность
     var duration: TimeInterval = 0
+    /// Часовой пояс
     var timeZone = TimeZone.current
     
     /// Инициализирует интервал с  начальной и конечной датой равной текущей дате, временным интервалом равным 0 и часовым поясом используемым системой (TimeZone.current)
@@ -27,6 +41,18 @@ public struct LACDateInterval {
     ///   - start: начальная дата
     ///   - end: конечная дата
     ///   - timeZone: часовой пояс
+    ///
+    /// Пример инициализации:
+    /// ```
+    /// // Текущая дата
+    /// let currentDate = Date()
+    /// // Начальная дата ранее текущей даты на 8 дней
+    /// let startDate = currentDate.addingTimeInterval(-8.days)
+    /// // Конечная дата через 7 дней после текущей
+    /// let endDate = currentDate.addingTimeInterval(7.days)
+    /// // Временной интервал продолжительностью 15 дней
+    /// let dateInterval = LACDateInterval(start: startDate, end: endDate, timeZone: timeZone)
+    /// ```
     public init(start: Date, end: Date, timeZone: TimeZone) {
         self.start = start
         self.end = end
@@ -39,6 +65,16 @@ public struct LACDateInterval {
     ///   - start: начальная дата
     ///   - duration: продолжительность
     ///   - timeZone: часовой пояс
+    ///
+    /// Пример инициализации:
+    /// ```
+    /// // Начальная дата ранее текущей даты на 8 дней
+    /// let startDate = Date().addingTimeInterval(-8.days)
+    /// // Часовой пояс +3 часа
+    /// let timeZone = TimeZone(secondsFromGMT: 3.hours) ?? TimeZone.current
+    /// // Временной интервал продолжительностью 15 дней
+    /// let dateInterval = LACDateInterval(start: startDate, duration: 15.days, timeZone: timeZone)
+    /// ```
     public init(start: Date, duration: TimeInterval, timeZone: TimeZone) {
         self.start = start
         self.end = start.addingTimeInterval(duration)
