246859b3461ef85d172c1719b3b684fd574c2bf3
Alexander Algashev <algashev@mail.ru>
2020-06-22 13:37:53 +0300

    Исправить документирование LACConfiguration [#172813191]

--- LimeAPIClient/LACConfiguration.swift
+++ LimeAPIClient/LACConfiguration.swift
@@ -21,32 +21,32 @@ import Foundation
 /// ```
 public struct LACConfiguration {
     /// Идентификатор приложения из маркета, например `limehd.ru.lite`.
-    /// Значение можно получить с помощью LACApp.id - (идентификатор пакета, который определяется ключом CFBundleIdentifier).
+    /// Значение можно получить с помощью LACApp.id - идентификатор пакета, который определяется ключом  [CFBundleIdentifier](https://developer.apple.com/documentation/bundleresources/information_property_list/cfbundleidentifier).
     let appId: String
-    /// Уникальрый ключи приложения
+    /// Уникальный ключ приложения
     let apiKey: String
-    /// Язык ожидаемого контента, который в указывается запросе
+    /// Язык ожидаемого контента, который указывается в запросе.
     ///
-    /// Описание см.:  https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/LanguageandLocaleIDs/LanguageandLocaleIDs.html#//apple_ref/doc/uid/10000171i-CH15
+    /// Описание см.: [Language and Locale IDs](https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPInternational/LanguageandLocaleIDs/LanguageandLocaleIDs.html#//apple_ref/doc/uid/10000171i-CH15).
     let language: String
-    /// Язык ожидаемого контента, который в указывается запросе в соответствии с ISO 639-1 Code / ISO 639-2 Code
+    /// Язык ожидаемого контента, который указывается в запросе в соответствии с [ISO 639-1 Code](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes) / [ISO 639-2 Code](https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes).
     lazy var languageDesignator: String = {
         let regionDesignatorDeleted = self.language.components(separatedBy: "_").first
         let scriptDesignatorDeleted = regionDesignatorDeleted?.components(separatedBy: "-").first
         return scriptDesignatorDeleted ?? ""
     }()
-    /// Идентификатор сессии, значение получают при успешном запросе сессии
+    /// Идентификатор сессии, значение получают при успешном запросе сессии.
     var sessionId = ""
-    /// Ссылка для редиректа на текущий поток, значение получают при успешном запросе сессии
+    /// Ссылка для редиректа на текущий поток, значение получают при успешном запросе сессии.
     var streamEndpoint = ""
-    /// Идентификатор группы канала, значение получают при успешном запросе сессии
+    /// Идентификатор группы канала, значение получают при успешном запросе сессии.
     var defaultChannelGroupId = ""
     
-    /// Инициализация конфигурации клиента  `LimeAPIClient`
+    /// Инициализация конфигурации клиента `LimeAPIClient`
     /// - Parameters:
-    ///   - appId: Идентификатор приложения из маркета, например `limehd.ru.lite`. Значение можно получить с помощью LACApp.id - (идентификатор пакета, который определяется ключом CFBundleIdentifier).
-    ///   - apiKey: Уникальрый ключи приложения
-    ///   - language: Язык ожидаемого контента, который в указывается запросе
+    ///   - appId: Идентификатор приложения из маркета, например `limehd.ru.lite`. Значение можно получить с помощью LACApp.id - идентификатор пакета, который определяется ключом [CFBundleIdentifier](https://developer.apple.com/documentation/bundleresources/information_property_list/cfbundleidentifier).
+    ///   - apiKey: Уникальный ключ приложения
+    ///   - language: Язык ожидаемого контента, который указывается в запросе
     ///
     /// Пример инициализации:
     /// ```
