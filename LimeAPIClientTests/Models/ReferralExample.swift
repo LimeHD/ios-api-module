d59d837e0ca1901a9967119ca088a3de8f984bfc
Alexander Algashev <algashev@mail.ru>
2020-08-26 15:18:39 +0300

    Добавить ReferralExample [#172813191]

---
+++ LimeAPIClientTests/Models/ReferralExample.swift
@@ -0,0 +1,18 @@
+//
+//  ReferralExample.swift
+//  LimeAPIClientTests
+//
+//  Created by Лайм HD on 26.08.2020.
+//  Copyright © 2020 Лайм HD. All rights reserved.
+//
+
+import Foundation
+
+let ReferralExample = """
+{
+    "current_time": "2020-08-26T15:04:28+03:00",
+    "share_url": "https://limepremium.page.link/?link=https://app.limehd.tv//referal%3Fuser_id=90935&apn=tv.limehd.premium",
+    "user_referral_url": "https://client.limehd.tv/profile/referrals/?user_id=90935",
+    "user_referral_url_expired_at": "2020-08-26T16:04:28+03:00"
+}
+"""
