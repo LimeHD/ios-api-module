5e186b1b1056435ebef68150ca117c7a61668a46
Alexander Algashev <algashev@mail.ru>
2020-08-26 15:13:41 +0300

    Добавить struct Referral [#172813191]

---
+++ LimeAPIClient/Models/Referral.swift
@@ -0,0 +1,16 @@
+//
+//  Referral.swift
+//  LimeAPIClient
+//
+//  Created by Лайм HD on 26.08.2020.
+//  Copyright © 2020 Лайм HD. All rights reserved.
+//
+
+import Foundation
+
+public struct Referral: Decodable, Equatable {
+    public let currentTime: String
+    public let shareUrl: String
+    public let userReferralUrl: String
+    public let userReferralUrlExpiredAt: String
+}
